8143e3e7808e74027a9867a1d8a56cfdcb7559c2
lokesh9350 <lokeshsangdoya@gmail.com>
2026-06-06 18:19:46 +0530

    libraryManager v01

---
+++ libraryManager.py
@@ -0,0 +1,18 @@
+def menu():
+    print("\n" * 2)
+
+    print("===== LIBRARY MANAGEMENT SYSTEM =====")
+
+    print("1. Add Book")
+    print("2. View Books")
+    print("3. Search Book")
+    print("4. Delete Book")
+    print("5. Exit")
+
+
+def main():
+    menu()
+
+
+if __name__ == "__main__":
+    main()
