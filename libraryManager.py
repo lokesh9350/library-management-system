3fb258e688409c893e69d6a87071f4a9f15f80c6
lokesh9350 <lokeshsangdoya@gmail.com>
2026-06-06 18:34:58 +0530

    libraryManager v02 - Added Add Book and View Books

--- libraryManager.py
+++ libraryManager.py
@@ -2,16 +2,59 @@ def menu():
     print("\n" * 2)
 
     print("===== LIBRARY MANAGEMENT SYSTEM =====")
-
     print("1. Add Book")
     print("2. View Books")
-    print("3. Search Book")
-    print("4. Delete Book")
-    print("5. Exit")
+    print("3. Exit")
+
+
+def add_book(ids, names, authors):
+    bid = input("Enter Book ID: ")
+    bname = input("Enter Book Name: ")
+    author = input("Enter Author Name: ")
+
+    ids.append(bid)
+    names.append(bname)
+    authors.append(author)
+
+    print("Book Added Successfully")
+
+
+def view_books(ids, names, authors):
+    if len(ids) == 0:
+        print("No Books Available")
+        return
+
+    print("\n===== BOOK LIST =====")
+
+    for i in range(len(ids)):
+        print(f"\nBook {i+1}")
+        print("Book ID    :", ids[i])
+        print("Book Name  :", names[i])
+        print("Author     :", authors[i])
 
 
 def main():
-    menu()
+    ids = []
+    names = []
+    authors = []
+
+    while True:
+        menu()
+
+        ch = int(input("Enter Choice: "))
+
+        if ch == 1:
+            add_book(ids, names, authors)
+
+        elif ch == 2:
+            view_books(ids, names, authors)
+
+        elif ch == 3:
+            print("Thank You")
+            break
+
+        else:
+            print("Invalid Choice")
 
 
 if __name__ == "__main__":
